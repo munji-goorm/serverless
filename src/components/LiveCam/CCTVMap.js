45f84862805849321c657fd777f56a1ae0fbad86
oduodg <werewildfire@gmail.com>
2022-11-26 13:04:57 +0900

    ⚡️ CCTV 영상을 새창으로 띄움

--- src/components/LiveCam/CCTVMap.js
+++ src/components/LiveCam/CCTVMap.js
@@ -34,54 +34,15 @@ export const CCTVMap = () => {
 			});
 
 			let cctv_Url = CCTVData[i].cctv_Url;
-			// content HTMLElement 생성
-			var content = document.createElement('div');
-			// 닫기 버튼 생성
-			var closeBtn = document.createElement('button');
-			closeBtn.style.backgroundColor = "#31333E";
-			closeBtn.style.color = "white";
-			closeBtn.style.position = "relative";
-			closeBtn.style.bottom = "-1.7rem";
-			closeBtn.style.left = "18.3rem";
-			closeBtn.style.border = "solid #24252D";
-			closeBtn.style.width = "1.3rem";
-			closeBtn.style.height = "1.3rem";
-			closeBtn.style.display = "flex";
-			closeBtn.style.alignItems = "center";
-			closeBtn.style.justifyContent = "center";
-			closeBtn.appendChild(document.createTextNode('x'));
-			content.appendChild(closeBtn);
-			// cctv iframe 생성
-			let video = document.createElement('iframe');
-			video.width = "320";
-			video.height = "300";
-			video.frameborder= "0";
-			video.src = cctv_Url;
-			content.appendChild(video);
-
-			// 닫기 이벤트 추가
-			closeBtn.onclick = function () {
-				overlay.setMap(null);
-			};
-
-			// 마커 위에 커스텀오버레이를 표시합니다
-			// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-			let overlay = new kakao.maps.CustomOverlay({
-				content: content,
-				map: map,
-				position: marker.getPosition()
-			});
-			overlay.setMap(null); // 마커를 클릭하기 전에는 표시하지 않습니다
-			// 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
 			kakao.maps.event.addListener(marker, 'click', function () {
-				overlay.setMap(map);
+				let width = 320;
+				let height = 300;
+				let left = (window.screen.width / 2) - (320 -2);
+				let top = (window.screen.height / 2) - (300 -2)
+				const windowFeatures = `left=${left},top=${top},width=${width},height=${height}`;
+				window.open(cctv_Url, "_blank", windowFeatures); // 새창에서 열림
 				map.setCenter(marker.getPosition()); // cctv 마커 클릭 시 마커 위치로 중심좌표 이동
 			});
-
-			// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
-			function closeOverlay() {
-				overlay.setMap(null);
-			}
 		}
 	});
 
