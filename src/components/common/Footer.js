04dd0b83f45b78d9f71ddc03ac0112b98be9c989
oduodg <werewildfire@gmail.com>
2022-12-06 09:11:36 +0900

    🎨 notion 링크 추가

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
 					<div className='text-2xl ml-[0.5rem]' style={{ fontFamily: "LeeSeoyun" }}>만든 사람</div>
 				</div>
 				<div className='text-lg py-[0.3rem]' style={{ fontFamily: "LeeSeoyun" }}>
-					예쁘고 귀엽게 봐주세요. 감사합니다 :) <br/>
+					예쁘고 귀엽게 봐주세요. 감사합니다 :) <br />
 					<span className='text-sm text-[#878787]'> 이름을 클릭하면 깃허브 링크로 이동합니다.</span>
 				</div>
 			</div>
@@ -20,10 +20,10 @@ export default function Footer() {
 			<div className='w-[63rem] h-[9rem] flex flex-row justify-between'>
 				<div className='flex flex-row w-[27rem] justify-between'>
 					<div className=''>
-					<a href='https://github.com/oduodg' target="_blank" rel="noreferrer">
-						<div className='mb-[0.2rem] text-lg hover:text-[#8A67AB] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
-							Eun <br /> Lee
-						</div>
+						<a href='https://github.com/oduodg' target="_blank" rel="noreferrer">
+							<div className='mb-[0.2rem] text-lg hover:text-[#8A67AB] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
+								Eun <br /> Lee
+							</div>
 						</a>
 						<div className='flex items-center'>
 							<div className='text-xs'>
@@ -32,24 +32,24 @@ export default function Footer() {
 						</div>
 					</div>
 					<div className=''>
-					<a href='https://github.com/ksee1230' target="_blank" rel="noreferrer">
-						<div className='mb-[0.2rem] text-lg hover:text-[#548164] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
-							Kyungsoo <br /> Park
-						</div>
+						<a href='https://github.com/ksee1230' target="_blank" rel="noreferrer">
+							<div className='mb-[0.2rem] text-lg hover:text-[#548164] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
+								Kyungsoo <br /> Park
+							</div>
 						</a>
 					</div>
 					<div className=''>
-					<a href='https://github.com/Zemni98' target="_blank" rel="noreferrer">
-						<div className='mb-[0.2rem] text-lg hover:text-[#5081A9] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
-							Jaemin <br /> Shin
-						</div>
+						<a href='https://github.com/Zemni98' target="_blank" rel="noreferrer">
+							<div className='mb-[0.2rem] text-lg hover:text-[#5081A9] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
+								Jaemin <br /> Shin
+							</div>
 						</a>
 					</div>
 					<div className=''>
-					<a href='https://github.com/soyeonnn' target="_blank" rel="noreferrer">
-						<div className='mb-[0.2rem] text-lg hover:text-[#C29343] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
-							Soyeon <br /> Hwang
-						</div>
+						<a href='https://github.com/soyeonnn' target="_blank" rel="noreferrer">
+							<div className='mb-[0.2rem] text-lg hover:text-[#C29343] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
+								Soyeon <br /> Hwang
+							</div>
 						</a>
 					</div>
 				</div>
@@ -67,7 +67,9 @@ export default function Footer() {
 							<a href='https://github.com/munji-goorm' target="_blank" rel="noreferrer">
 								<GithubIcon className='mx-[0.3rem]' />
 							</a>
+							<a href='https://oduodg.notion.site/39490b032260464fb7a5160a5867b4cf' target="_blank" rel="noreferrer">
 								<NotionIcon className='mx-[0.3rem]' />
+							</a>
 						</div>
 					</div>
 					<div className='text-sm text-center text-[#838383] pt-[1rem]'>
