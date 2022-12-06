1720ac261bc0d4a1de0d401f03b9ec83b8e96cca
oduodg <werewildfire@gmail.com>
2022-12-06 09:13:30 +0900

    ✨ 최근 검색어 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,39 +3,45 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header, Footer } from './components/common';
 import { AirPollution, LiveCam, OntheMap } from './pages';
 import { useEffect, useState } from 'react';
-import { GetLocation, GetMainData } from './apis';
+import { GetMainData } from './apis';
 
 function App() {
-  let location = GetLocation(); //geolocation API를 통해 현재 위치 가져오기
-  const [coord, setCoord] = useState({ //위도, 경도
-    lat: 37.564639, //서울시 중구 측정소 위도
-    lng: 126.975961, //서울시 중구 측정소 경도
-  });
-  const [addr, setAddr] = useState("중구 서소문동"); //지역(xx구 oo동)
+  let xCoord = 37.5607179; //서울시 중구 의주로2가 위도
+  let yCoord = 126.9695899; //서울시 중구 의주로2가 경도
+  let currShortAddr = "중구 의주로2가";
+  let currFullAddr = "서울특별시 중구 의주로2가";
 
-  /* 현재 위치로 위도/경도 변경 */
-  useEffect(() => {
-    if (location) {
-      setCoord({
-        lat: location.lat,
-        lng: location.lng
-      });
-    }
-  }, [location]);
-  
-  let stationData = GetMainData(coord.lat, coord.lng); //현재 위치에서 근접측정소의 대기오염정보
-  
+  if (localStorage.getItem('userXCoord')) {
+    xCoord = parseFloat(localStorage.getItem('userXCoord'));
+    yCoord = parseFloat(localStorage.getItem('userYCoord'));
+    currShortAddr = localStorage.getItem('userShortAddr');
+    currFullAddr = localStorage.getItem('userFullAddr');
+  }
+
+  const [coord, setCoord] = useState({ //위도, 경도
+    lat: xCoord,
+    lng: yCoord,
+  });
+
+  const [shortAddr, setShortAddr] = useState(currShortAddr); //지역(xx구 oo동)
+  const [fullAddr, setFullAddr] = useState(currFullAddr);
+ 
+  let stationData = GetMainData(fullAddr); //현재 위치에서 근접측정소의 대기오염정보
+
   /* 현재 지역 변경 */
   useEffect(() => {
-    setAddr(stationData.stationInfo.shortAddr);
+    setShortAddr(stationData.stationInfo.shortAddr);
+    if (localStorage.getItem('userFullAddr')) {
+      localStorage.setItem('userShortAddr', stationData.stationInfo.shortAddr);
+    }
   }, [stationData]);
 
   return (
     <BrowserRouter>
-      <Header addr={addr} setAddr={setAddr} setCoord={setCoord} />
+      <Header shortAddr={shortAddr} setShortAddr={setShortAddr} setFullAddr={setFullAddr} coord={coord} setCoord={setCoord} />
       <Routes>
         <Route exact path="/" element={
-          <AirPollution stationData={stationData} addr={addr}/>
+          <AirPollution stationData={stationData}/>
         } />
         <Route path="/airmap" element={
         <OntheMap coord={coord}/>} />
--- src/components/common/SearchBox.js
+++ src/components/common/SearchBox.js
@@ -1,11 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg';
 import axios from 'axios';
+import { ReactComponent as DeleteIcon } from '../../assets/icons/delete.svg';
 
-export const SearchBox = ({ setAddr, setCoord, setSearchBtn }) => {
+export const SearchBox = ({ setShortAddr, setFullAddr, setCoord, setSearchBtn }) => {
 	/* 특정 시간 동안 연속적으로 호출되는 이벤트 중 마지막 이벤트만 호출  */
-	//callback: 일정 시간이 지난 후 실행되는 함수, delay: 지연 시간
-	const debounce = (callback, delay) => {
+	const debounce = (callback, delay) => {//callback: 일정 시간이 지난 후 실행되는 함수, delay: 지연 시간
 		let timer;
 		return (...args) => {
 			clearTimeout(timer);
@@ -13,7 +13,81 @@ export const SearchBox = ({ setAddr, setCoord, setSearchBtn }) => {
 		}
 	}
 
+	// localStorage.clear();
+	/**최근 검색어 */
+	//localStorage의 데이터를 꺼내서 배열로 변환한다.
+	let shortAddrArr = JSON.parse(localStorage.getItem('shortAddr'));
+	if (shortAddrArr === null) {
+		//최초 접속시 localStorage에 데이터가 없을 경우 새로운 배열 생성하기
+		shortAddrArr = [];
+	}
+
+	let fullAddrArr = JSON.parse(localStorage.getItem('fullAddr'));
+	if (fullAddrArr === null) {
+		fullAddrArr = [];
+	}
+
+	let xCoordArr = JSON.parse(localStorage.getItem('xCoord'));
+	if (xCoordArr === null) {
+		xCoordArr = [];
+	}
+
+	let yCoordArr = JSON.parse(localStorage.getItem('yCoord'));
+	if (yCoordArr === null) {
+		yCoordArr = [];
+	}
+
+	const submitRecentWord = shortAddr => {
+		let idx;
+		for (let i=0; i<shortAddrArr.length; i++) {
+			if (shortAddrArr[i] === shortAddr) {
+				idx = i;
+				break;
+			}
+		}
+		
+		setFullAddr(fullAddrArr[idx]);
+		setShortAddr(shortAddrArr[idx]);
+		setCoord({
+			lat: xCoordArr[idx],
+			lng: yCoordArr[idx],
+		});
+		setSearchBtn(true);
+	}
+
+	//최근 검색어 삭제
+	const deleteRecentWord = shortAddr => {
+		//버튼 안보이게 처리
+		document.getElementById(shortAddr).style.display = 'none';
+		//삭제할 주소를 제외하고 새로운 배열을 생성한다.
+		//shortAddrArr = shortAddrArr.filter((items) => items !== shortAddr);
+		let idx;
+		for (let i=0; i<shortAddrArr.length; i++) {
+			if (shortAddrArr[i] === shortAddr) {
+				idx = i;
+				shortAddrArr.splice(i, 1);
+				break;
+			}
+		}
+		for (let i=0; i<fullAddrArr.length; i++) {
+			if (i === idx){
+				fullAddrArr.splice(i, 1);
+				xCoordArr.splice(i, 1);
+				yCoordArr.splice(i, 1);
+				break;
+			}
+		}
+		
+		//새로운 배열을 localStorage에 저장한다.
+		localStorage.setItem('shortAddr', JSON.stringify(shortAddrArr));
+		localStorage.setItem('fullAddr', JSON.stringify(fullAddrArr));
+		localStorage.setItem('xCoord', JSON.stringify(xCoordArr));
+		localStorage.setItem('yCoord', JSON.stringify(yCoordArr));
+	}
+
+
 	/**검색어 자동완성 */
+	const inputBox = useRef();
 	const [inputValue, setInputValue] = useState('');
 	const [isHaveInputValue, setIsHaveInputValue] = useState(false);
 	const [dropDownList, setDropDownList] = useState([]);
@@ -51,13 +125,51 @@ export const SearchBox = ({ setAddr, setCoord, setSearchBtn }) => {
 	const clickDropDownItem = clickedItem => {
 		setInputValue(clickedItem.shortAddr);
 		setIsHaveInputValue(false);
-		setAddr(clickedItem.shortAddr);
+		setShortAddr(clickedItem.shortAddr);
+		setFullAddr(clickedItem.fullAddr);
 		setCoord({
 			lat: clickedItem.xCoord,
 			lng: clickedItem.yCoord
 		});
 		setSearchBtn(true);
 		document.getElementById('drop-down-icon').classList.remove('rotate-180');
+		
+		/* 최근 검색어 저장 */
+		let newShortAddr = clickedItem.shortAddr;
+		let newFullAddr = clickedItem.fullAddr;
+		let newXCoord = clickedItem.xCoord;
+		let newYCoord = clickedItem.yCoord;
+		
+		//새로 검색한 주소를 추가한다.
+		shortAddrArr.unshift(newShortAddr); //배열의 맨 앞에 추가한다.
+		fullAddrArr.unshift(newFullAddr);
+		xCoordArr.unshift(newXCoord);
+		yCoordArr.unshift(newYCoord);
+		
+		//중복된 주소는 제거한다.
+		shortAddrArr = new Set(shortAddrArr);
+		fullAddrArr = new Set(fullAddrArr);
+		xCoordArr = new Set(xCoordArr);
+		yCoordArr = new Set(yCoordArr);
+		
+		//중복 제거된 set 자료형의 addrArr를 일반 배열로 변경한다.
+		shortAddrArr =[...shortAddrArr];
+		fullAddrArr =[...fullAddrArr];
+		xCoordArr = [...xCoordArr];
+		yCoordArr = [...yCoordArr];
+		
+		if (shortAddrArr.length > 5) { //최근 검색 5개까지만 저장
+			shortAddrArr.pop();
+			fullAddrArr.pop();
+			xCoordArr.pop();
+			yCoordArr.pop();
+		}
+		
+		//localStorage에 저장한다.
+		localStorage.setItem('shortAddr', JSON.stringify(shortAddrArr));
+		localStorage.setItem('fullAddr', JSON.stringify(fullAddrArr));
+		localStorage.setItem('xCoord', JSON.stringify(xCoordArr));
+		localStorage.setItem('yCoord', JSON.stringify(yCoordArr));
 	}
 
 	const handleDropDownKey = event => {
@@ -83,16 +195,42 @@ export const SearchBox = ({ setAddr, setCoord, setSearchBtn }) => {
 			<div className='w-[63rem] h-[25rem] flex flex-col items-center'>
 				<div className='flex items-center mt-[1.5rem] w-[58rem] h-[3rem] bg-[#f4f4f4] rounded-3xl'>
 					<SearchIcon className='ml-4' />
-					<input type="text" placeholder="지역으로 검색하세요."
+					<input
+						ref={inputBox}
+						type="text" placeholder="지역으로 검색하세요."
 						className='bg-[#f4f4f4] w-[53rem] outline-none ml-2'
 						value={inputValue}
 						onChange={changeInputValue}
 						onKeyUp={handleDropDownKey}
-					/>
+				/>
 				</div>
 				<div>
+					<div className='mt-[1rem] w-[58rem] h-[2rem] flex items-center text-lg'>
+						<div className='mr-2 text-[#878787]'>최근 검색</div>
+						{(localStorage.getItem('shortAddr') && localStorage.getItem('shortAddr') !== '[]')
+						? <div className='w-[53rem] flex overflow-x-auto'>
+							{JSON.parse(localStorage.getItem('shortAddr')).map((shortAddr, index) => {
+							return (
+								<button
+									key={index}
+									id={shortAddr}
+									className="flex items-center px-2 mx-1 bg-[#f4f4f4] text-[#777777] rounded-xl hover:border-2 break-normal"
+									onClick={() => {submitRecentWord(shortAddr)}}
+								>
+									{shortAddr}
+									<DeleteIcon 
+										className='inline ml-1 rounded hover:bg-red-400'
+										onClick={() => {deleteRecentWord(shortAddr)}} />
+								</button>
+							)
+						})}
+						</div>
+						:	<div className='text-[#878787]'>기록이 없습니다.</div>
+						}
+						
+					</div>
 					{isHaveInputValue && (
-						<div className='mt-[1rem] w-[58rem] h-[19.5rem] overflow-auto'>
+						<div className='mt-[1rem] w-[58rem] h-[15.5rem] overflow-auto'>
 							{dropDownList.map((dropDownItem, dropDownIndex) => {
 								return (
 									<div
